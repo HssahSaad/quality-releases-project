6b1fa15d1c10bedca532e8de1bf86e90cfc7735b
Hssah Alsharihi <168639436+HssahSaad@users.noreply.github.com>
2025-09-06 04:10:56 +0300

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,31 +1,34 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip_id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_linux_virtual_machine" "vm_instance" {
+  name                = var.vm_name
+  location            = var.location
+  resource_group_name = var.resource_group
   size                = "Standard_DS2_v2"
-  admin_username      = ""
-  network_interface_ids = []
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+
   admin_ssh_key {
-    username   = "hssah"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnxlSoVmRX01Be+F0ygG+rhSjvrg2+P3UVoCHXVZK6nxNixPX+DeM6asKtOngOnBxpcwrYt/Vk3lfpPLggN06wmngP8tpdnOStRLMYawMkI5cjhmnZ9ZPPGo9Kegg7oIoFQQ3MFve7/RpXVv41OzcPnoyVeDi+04SXbcyi15skkUA5OBGwmjzOIXEoaI6/RX/feH7m59S/o6KHrn7tVUm3FupHpLFgNsL0ZjJQwFyXMWc3TorsNg0ORprdY0FXLRZyTtqGIZKmINjVzQRVzgPocjhKKszMnJSXjaU/r501Ra2Z8pMIt3NGkzchuV+ERdPbky+aYqcysAX0NSvxi2rvsrMp7qcJBpC8lQxxVMXWjSI/k7uphLCcVxh2Bfx00SPaz+4v/vBgHNf2bmhAmp33OXvZDrH+pUHBqhPyVjo0cyEv1k0ZncsfA/nMLQC9bprYHGIvNSckPmM/0aUtBkeUafgl8BXdXr4TkDiL7l376ZoMoxPTtodh5itUbvy5Cd8= hssah@SandboxHost-638926817756757254"
+    username   = var.admin_username
+    public_key = var.admin_ssh_public_key
   }
+
   os_disk {
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
